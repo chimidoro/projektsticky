fd7352313e6af9ddec9a0c0f687009b9efce365d
chimidoro <33393205+chimidoro@users.noreply.github.com>
2017-11-07 17:03:07 +0100

    bewerben

--- bewerben.php
+++ bewerben.php
@@ -3,68 +3,89 @@
             
                    
                 <div class="formbox">            
-                        <form action="index.php" method="post">
-                            <div class="card-header" style="margin-bottom: 2%"><h4><u>Thema eintragen</u></h4></div>
+                        <form action="bewerben.php" method="post">
+                            <div class="card-header" style="margin-bottom: 2%"><h4><u>Bewerbung auf ein Seminar- oder Abschlussarbeitsthema</u></h4></div>
                             <table>
-
                              <tr>
                                  <td> <label for="Kategorie"><b>Kategorie:</b></label> </td>    
                                  <td> <input type="radio" name="kategorie1" id="inlineRadio1" value="option1"> Seminarthema </input>    </td> 
                                    <td>  <input type="radio" name="kategorie1" id="inlineRadio1" value="option2"> Abschlussarbeit </input> 
                                  </td> 
                             </tr>
-                             </table>
-                            
-                             <table>
-                             <tr>
-                                 <td><label for="Termine"><b>Bewerbungsfristen</b></label> </td>
-                                 <td><input type="text" class="form-control" id="Starttermin" placeholder="TT.MM.YYYY">
-                                    <span>Starttermin</span> </td>
-                                 <td><input type="text" class="form-control" id="Endtermin"  placeholder="TT.MM.YYYY">
-                                     <span>Endtermin</span> 
-                                 </td>
-                             </tr>
                             </table>
-                             <table>
+        
+                            <table>
+
                              <tr>
-                                 <td style="width: 17%;" ><label for="inputTitel"><b>Titel:</b></label>  </td> 
-                                 <td><input type="text" class="form-control" id="inputTitel" placeholder="Titel"> </td> 
-                             </tr>
-
-                              <tr>
-                                 <td><label for="Vorraussetzung" ><b>Zulassungsvorraussetzung:</b></label>  </td> 
-                                 <td><input type="text" class="form-control" name="Vorraussetzng" placeholder="(Optional)" id="form-control"></td>                             
-                             </tr>
-                             
-                                <tr>
-                                   <td><label for="bevStudiengang"><b>bevorzugter Studiengang:</b> </label></td>
-                                    <td><select class="form-control" id="bevStudiengang">
-                                    <option>Keiner</option>
-                                    <option>Betriebswirtschaftslehre</option>
+                                   <td><label for="inputStudiengang"><b>Studiengang:</b> </label></td>
+                                    <td><select class="form-control" id="Studiengang">
+                                    <option></option>
                                     <option>Wirtschaftsinformatik</option>
+                                    <option>Betriebswirtschaftslehre</option>
                                     <option>Volkswirtschaftslehre</option>
-                                    <option>Wirtschaftsp√§dagogik</option>
-                                        </select></td>
-                                </tr>
-
-                              <tr>
-                                 <td><label for="Tags"><b>Tags: </b></label>  </td> 
-                                 <td><input type="text" class="form-control" name="Tags" id="form-control"></td>                             
-                             </tr>
-
-                               <tr>
-                                   <td><label for="inputVerfahren"><b>Verfahren:</b> </label></td>
-                                    <td><select class="form-control" id="Verfahren">
-                                    <option>Windhund-Verfahren</option>
-                                    <option>Bewerbungsverfahren</option>
-                                    <option>Belegungswunsch-Verfahren</option>
+                                    <option>Wirtschaftsp‰dagogik</option>
                                     </select></td>
                                 </tr>
+
+                                <tr>
+                                 <td style="width: 17%;"><label for="inputSemesteranzahl"><b>Aktuelles Fachsemester:</b></label>  </td> 
+                                 <td><input type="text" class="form-control" id="inputSemesteranzahl" placeholder="Bspw.: 5 "> </td> 
+                               </tr>
+
+                                <tr>
+                                 <td style="width: 17%;" ><label for="inputCreditsranzahl"><b>Creditsanzahl:</b></label>  </td> 
+                                 <td><input type="text" class="form-control" id="inputCreditsranzahl" placeholder="Anzahl der Credits"> </td> 
+                               </tr>
+
+                          
                                <tr>
-                                   
-                                 <td><label for="inputBeschreibung"><b>Beschreibung:</b></label>  </td> 
-                                 <td><textarea class="form-control" name="beschreibung" id="exampleFormControlTextarea1" rows="8" placeholder="Beschreibung des Themas"></textarea></td>                             
-                             </tr>
+                                   <td><label for="inputoPhase"><b>Abgeschlossene O-Phase:</b> </label></td>
+                                    <td><select class="form-control" id="oPhase">
+                                    <option>Ja</option>
+                                    <option>Nein</option>
+                                    </select></td>
+                                </tr>
+
+                               <tr>
+                                   <td><label for="inputTeilnahme"><b>Bereits an ein Seminar/Abschlussarbeit teilgenommen aber nicht abgegeben:</b> </label></td>
+                                    <td><select class="form-control" id="Teilnahme">
+                                    <option>Ja</option>
+                                    <option>Nein</option>
+                                    </select></td>
+                                </tr>
+
+                               <tr>
+                                   <td><label for="inputoPhase"><b>Belegwunsch 1:</b> </label></td>
+                                    <td><select class="form-control" id="b1">
+                                    <option></option>
+                                    <option>Burger</option>
+                                    <option>Pizza</option>
+                                    <option>Salat</option>
+                                    </select></td>
+                                </tr>
+
+                               <tr>
+                                   <td><label for="inputoPhase"><b>Belegwunsch 2:</b> </label></td>
+                                    <td><select class="form-control" id="b1">
+                                    <option></option>
+                                    <option>Burger</option>
+                                    <option>Pizza</option>
+                                    <option>Salat</option>
+                                    </select></td>
+                                </tr>
+
+                               <tr>
+                                   <td><label for="inputoPhase"><b>Belegwunsch 3:</b> </label></td>
+                                    <td><select class="form-control" id="b1">
+                                    <option></option>
+                                    <option>Burger</option>
+                                    <option>Pizza</option>
+                                    <option>Salat</option>
+                                    </select></td>
+                                </tr>
+
+
+
                              <tr>
                               
                               <td>    <div class="card-header" style="margin-bottom: 2%">Thema eintragen</div> </td>       
