7b17910791707fc56622e679ed3eb8b595f653b7
Duc Nguyen <duc-anh95@hotmail.de>
2017-12-07 16:48:52 +0100

    stick ordner gelöscht

--- stick/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost"; // Adresse des Datenbankservers, meistens localhost
-$user = "stickywebs"; // Ihr MySQL Benutzername
-$pass = "shyre112"; // Ihr MySQL Passwort
-$db = "stickywebs"; // Name der Datenbank
-
-$link = mysql_connect($host, $user, $pass);
-mysql_select_db($db, $link);
-?>
--- stick/test/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('db.php');
-
-require_once("controllers/controller.php");
-$controllers = new Controller();
-$controllers->invoke();
-?>
